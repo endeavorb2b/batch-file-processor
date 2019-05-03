6d3330466807069937ab6bec31405fa65fd52c1b
jlaird <jlaird@endeavorb2b.com>
2019-05-03 15:25:50 +0000

    Added commments

--- scripts/import-documents.sh
+++ scripts/import-documents.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Usage:    ./getdocuments.sh fileName
-# Example:  ./getdocuments.sh nfn.1.csv
+# Usage:    ./run.sh import-documents.sh fileName
+# Example:  ./run.sh import-documents.sh nfn.1.csv
 
-#mediaPath="files/base/scomm/nfn/document/2015/03"
+
 document=$1
-
+# originalSource="https://waterworld.com/content/leftcolumn/whitepaper/Chlor-Alkali_White%20Paper.pdf"
 originalSource=$(awk -F "|" '{print $1}' <<< $document)
+# mediaPath="files/base/scomm/nfn/document/2015/03"
 mediaPath=$(awk -F "|" '{print $2}' <<< $document)
+# localfilename="photometric_colorimetricanalyze_v0021.pd"
 localfilename=$(awk -F "|" '{print $3}' <<< $document)
 
 
